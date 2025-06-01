48e0678917c4b487b650c0879175e54ec6998087
neatstranger <39034037+neatstranger@users.noreply.github.com>
2025-06-01 16:21:33 -0700

    Update movieFileClose.sh

--- root_overlay/obsc/scripts/movieFileClose.sh
+++ root_overlay/obsc/scripts/movieFileClose.sh
@@ -13,26 +13,22 @@ fi
 if [ -f "$KEY_FILE" ]; then
   echo "Importing GPG key from $KEY_FILE..."
   gpg --import "$KEY_FILE"
+
+  echo "Encrypting $FILE_NAME..."
+  gpg --batch --yes --trust-model always \
+      --output "$FILE_NAME.gpg" \
+      --encrypt --recipient Obscura_Key \
+      "$FILE_NAME"
+
+  if [ $? -eq 0 ]; then
+    echo "Encryption successful. Deleting original file..."
+    rm -f "$FILE_NAME"
+    FILE_NAME=$FILE_NAME.gpg
+  else
+    echo "Error during encryption. Original file not deleted."
+  fi
 else
   echo "No GPG key file found at '$KEY_FILE'. Assuming key was previously imported."
 fi
 
-# Get recipient from the keyring (assumes the key has a usable UID)
-RECIPIENT=$(gpg --list-keys --with-colons | awk -F: '/^uid:/ { print $10; exit }')
-
-if [ -z "$RECIPIENT" ]; then
-  echo "No valid GPG recipient found. Skipping encryption."
-  exit 0
-fi
-
-# Encrypt the file
-echo "Encrypting $FILE_NAME..."
-gpg --output "$FILE_NAME.gpg" --encrypt --recipient "$RECIPIENT" "$FILE_NAME"
-
-# Verify encryption succeeded before deleting
-if [ $? -eq 0 ]; then
-  echo "Encryption successful. Deleting original file..."
-  rm -f "$FILE_NAME"
-else
-  echo "Error during encryption. Original file not deleted."
-fi
+logger "Proceeding with $FILE_NAME upload....."
