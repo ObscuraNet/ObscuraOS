e5d1370e17e247f4177f6fe5cf22408a2ff5b90e
neatstranger <39034037+neatstranger@users.noreply.github.com>
2025-06-01 16:25:48 -0700

    Moving away from encryption for now.

--- root_overlay/obsc/scripts/movieFileClose.sh
+++ root_overlay/obsc/scripts/movieFileClose.sh
@@ -9,26 +9,4 @@ if [ ! -f "$FILE_NAME" ]; then
   exit 1
 fi
 
-# Import the key if the file exists
-if [ -f "$KEY_FILE" ]; then
-  echo "Importing GPG key from $KEY_FILE..."
-  gpg --import "$KEY_FILE"
-
-  echo "Encrypting $FILE_NAME..."
-  gpg --batch --yes --trust-model always \
-      --output "$FILE_NAME.gpg" \
-      --encrypt --recipient Obscura_Key \
-      "$FILE_NAME"
-
-  if [ $? -eq 0 ]; then
-    echo "Encryption successful. Deleting original file..."
-    rm -f "$FILE_NAME"
-    FILE_NAME=$FILE_NAME.gpg
-  else
-    echo "Error during encryption. Original file not deleted."
-  fi
-else
-  echo "No GPG key file found at '$KEY_FILE'. Assuming key was previously imported."
-fi
-
 logger "Proceeding with $FILE_NAME upload....."
